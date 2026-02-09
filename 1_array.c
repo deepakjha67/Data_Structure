975424ab2cdd3e9f6303c3be8c4be557b94cc77c
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-02-09 21:02:48 +0530

    Refactor array input and display logic

--- 1_array.c
+++ 1_array.c
@@ -1,28 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int n, i;
-
-    // 1. Ask for the size of the array
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
-
-    // 2. Declaration
-    // We define the array type (int) and size [n]
-    int numbers[n]; 
-
-    // 3. Taking input from the user (Initialisation via loop)
-    printf("Enter %d integers:\n", n);
-    for(i = 0; i < n; i++) {
-        printf("Element %d: ", i + 1);
-        scanf("%d", &numbers[i]);
+    int arr[50], size, i;
+    printf("Enter thr size of the array: ");
+    scanf("%d" , &size);
+    
+    printf("Enter the %d elements of array: \n" , size);
+    for(i=0; i<size; i++) {
+        scanf("%d" , &arr[i]);
+        }
+    
+      
+        printf("\nElements of the array are: \n");
+        for(i=0; i<size; i++) {
+            printf("%d", arr[i]);
+            }
+        
+        return 0;
     }
-
-    // 4. Displaying the array elements
-    printf("\nThe array elements are: ");
-    for(i = 0; i < n; i++) {
-        printf("%d ", numbers[i]);
-    }
-
-    return 0;
-}
