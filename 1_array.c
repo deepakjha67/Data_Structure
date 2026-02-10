da1568aeb4604bbe0809e780c416e4d72b75048d
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-02-10 23:47:07 +0530

    Improve array input prompts and output display
    Updated array input/output handling and formatting.

--- 1_array.c
+++ 1_array.c
@@ -1,20 +1,17 @@
-#include <stdio.h>
-
-int main() {
-    int arr[50], size, i;
-    printf("Enter thr size of the array: ");
+#include<stdio.h>
+int main () {
+    int size, arr[50],i;
+    printf("Enter the size of the array:  ");
     scanf("%d" , &size);
     
-    printf("Enter the %d elements of array: \n" , size);
     for(i=0; i<size; i++) {
+        printf("arr[%d]" , i);
         scanf("%d" , &arr[i]);
-        }
     
-      
-        printf("\nElements of the array are: \n");
+        }
+        
         for(i=0; i<size; i++) {
             printf("%d", arr[i]);
-            }
-        
-        return 0;
-    }
+            }     
+    return 0;
+}
