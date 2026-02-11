035424665f83867370d09d55df50f2512001def3
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-02-11 19:42:06 +0530

    Update 1_array.c

--- 1_array.c
+++ 1_array.c
@@ -1,17 +1,20 @@
 #include<stdio.h>
-int main () {
-    int size, arr[50],i;
-    printf("Enter the size of the array:  ");
-    scanf("%d" , &size);
-    
+int main() {
+    int arr[50], i, size;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
     for(i=0; i<size; i++) {
-        printf("arr[%d]" , i);
+        printf("A[%d]", i);
         scanf("%d" , &arr[i]);
-    
-        }
-        
-        for(i=0; i<size; i++) {
-            printf("%d", arr[i]);
-            }     
+
+    }
+
+    printf("The elements of the array are: ");
+    for (i=0; i<size; i++) {
+        printf("%d" , arr[i]);
+    }
     return 0;
+
 }
